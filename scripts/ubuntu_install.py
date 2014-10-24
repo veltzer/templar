a2cff6e50316355a644d282d5e189e9a0e28e97e
Mark Veltzer <mark@veltzer.net>
2014-10-24 21:37:51 +0300

    making deb package

---
+++ scripts/ubuntu_install.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''
+this script will install all the required packages that you need on
+ubuntu to compile and work with this package.
+'''
+
+import subprocess # for check_call
+
+packs=[
+	# for scripts
+	'python3', # for the python scripts
+
+	# mako templating (current templating)
+	'python3-mako', # mako for python 3 (we are not really using it)
+	'python-mako-doc', # documentation for the template preprocessor
+]
+
+args=[
+	'sudo',
+	'apt-get',
+	'install',
+	'--assume-yes'
+]
+args.extend(packs)
+subprocess.check_call(args)
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import distutils.core # for setup
+import setuptools # for find_packages
+import subprocess # for check_output
+
+distutils.core.setup(
+	name='templar',
+	description='Templating engine for python',
+	long_description='Templating engine for python (long description)',
+	author='Mark Veltzer',
+	author_email='mark@veltzer.net',
+	maintainer='Mark Veltzer',
+	maintainer_email='mark@veltzer.net',
+	keywords=[
+		'mako',
+		'templating',
+		'python',
+	],
+	url='https://www.veltzer.net/templar',
+	license='LGPL',
+	platforms=[
+		'ALL',
+	],
+	packages=setuptools.find_packages(),
+	version=subprocess.check_output(['git','describe']).decode().rstrip(),
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: LGPL',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Topic :: Software Development :: Building',
+		'Topic :: Software Development :: Libraries',
+		'Topic :: Utilities',
+	]
+)
