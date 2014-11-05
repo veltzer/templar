43cdab083a91e65bcadd101da8b7604636586bcc
Mark Veltzer <mark@veltzer.net>
2014-11-06 01:05:58 +0200

    added type for script

--- templar/attr.py
+++ templar/attr.py
@@ -60,7 +60,7 @@ class Attr(object):
 		cls.project_name=os.path.basename(os.getcwd())
 
 		if 'project_google_analytics_tracking_id' in cls.__dict__:
-			cls.project_google_analytics_snipplet='''<script>
+			cls.project_google_analytics_snipplet='''<script type="text/javascript">
 (function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
 (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
