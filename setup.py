fb784b2a31dcaf1952e1db531774e48527a964b9
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-08 20:50:55 +0200

    12

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import distutils.core # for setup
 
 distutils.core.setup(
 	name='templar',
-	version='11',
+	version='12',
 	description='Easy templating tool long description (TBD)',
 	long_description='Templating solution for programmers',
 	author='Mark Veltzer',
