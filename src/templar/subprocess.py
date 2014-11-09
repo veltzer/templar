9d0e1416deaeaafb796b740af72fad9b9346cecd
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-09 10:35:38 +0200

    some TODO items

--- src/templar/subprocess.py
+++ src/templar/subprocess.py
@@ -4,6 +4,11 @@ A module which wraps subprocess
 Why do we need this?
 This is a single place where activation of processes takes place and so we could
 centrally debug everything that is going on.
+
+TODO:
+- in the case where debug is not needed so output to the screen should be
+minimized aggregate the output of the commands
+and output them in case of error or debug.
 '''
 
 import subprocess # for check_call, DEVNULL
@@ -20,6 +25,7 @@ def check_call(l):
 	else:
 		subprocess.check_call(l, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
+'''check_output wrapper'''
 def check_output(l):
 	templar.debug.process(l)
 	if opt_debug:
