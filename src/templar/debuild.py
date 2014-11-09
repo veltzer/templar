61f79bbe610b7e14e037f14fd584276b4b989752
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-09 02:53:17 +0200

    no more defs for makefiles

--- src/templar/debuild.py
+++ src/templar/debuild.py
@@ -2,13 +2,12 @@
 This module knows how to build debian packages using the debuild(1) command.
 '''
 
-import subprocess # for call
-import templar.wrappers.debuid # for run
-import os # for unlink
-import glob # for glob
 import templar.api # for load_and_populate
 import templar.git # for clean
 import templar.make # for make
+import glob # for glob
+import os # for unlink, mkdir
+import templar.wrappers.debuild # for run
 
 def run():
 	d=templar.api.load_and_populate()
@@ -16,7 +15,7 @@ def run():
 	templar.make.make('templar')
 	for f in glob.glob('../{0}_*'.format(d.deb_pkgname)):
 		os.unlink(f)
-	templar.wrappers.debuid(['debuild','-S'])
+	templar.wrappers.debuild.run(['debuild','-S'])
 	os.mkdir(d.deb_build_source)
 	'''
 	$(Q)mv ../$(tdefs.deb_pkgname)_* $(tdefs.deb_build_source)
