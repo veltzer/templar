e4fe8ccdb3ec145ed8dc225313406a4aadb909cf
Mark Veltzer <mark.veltzer@gmail.com>
2016-07-24 14:21:21 +0300

    more debug stuff

--- src/templar/debug.py
+++ src/templar/debug.py
@@ -5,12 +5,13 @@ centralized debugging module
 import sys # for stderr
 
 # do you want to see the commands?
-opt_show_commands=True
+opt_show_commands=False
+opt_show_message=False
 
 def process(l, tag):
 	if opt_show_commands:
 		print('executing [{0}] [{1}]...'.format(l, tag), file=sys.stderr)
 
 def debug(s):
-	if opt_show_commands:
+	if opt_show_messages:
 		print(s, file=sys.stderr)
