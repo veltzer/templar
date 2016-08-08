6379eb00d20598616ea211787efab0201e5a08be
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-08 17:12:53 +0300

    local config script is in

---
+++ scripts/apply_local_config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git config --local user.signingkey 73C128F9
+git config --local core.editor vim
+git config --local user.name "Mark Veltzer"
+git config --local user.email "mark.veltzer@gmail.com"
