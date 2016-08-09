d900a97a80ba7ef773f8f2a38589acc8dd6411ed
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-09 11:45:12 +0300

    better install process

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -5,40 +5,38 @@ this script will install all the required packages that you need on
 ubuntu to compile and work with this package.
 '''
 
-import subprocess # for check_call
+import subprocess # for check_call, DEVNULL
+import yaml # for load
+import os.path # for isfile
 
-packs=[
-	# for scripts
-	'python3', # for the python scripts
-	'python3-all', # for the python scripts
-	'dh-python', # for packaging helpers
+APT_FILE='apt.yaml'
+def install_apt():
+	# if the file is not there it is NOT an error
+	if not os.path.isfile(APT_FILE):
+		return
+	print('installing from {0}...'.format(APT_FILE))
+	with open(APT_FILE, 'r') as stream:
+		o=yaml.load(stream)
+	packs=[ x['name'] for x in o['packages'] ]
+	args=[
+		'sudo',
+		'apt-get',
+		'install',
+		'--assume-yes'
+	]
+	args.extend(packs)
+	subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-	# for installation:
-	'python-setuptools-doc', # for packaging
-	'python3-setuptools', # for packaging
-	'python-setuptools', # for packaging
-	'python3-setuptools-git', # for packaging
-	'python-setuptools-git', # for packaging
+NODE_FILE='package.json'
+def install_node():
+	# if the file is not there it is NOT an error
+	if not os.path.isfile(NODE_FILE):
+		return
+	print('installing from {0}...'.format(NODE_FILE))
+	subprocess.check_call([
+		'npm',
+		'install',
+	], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-	# mako templating (current templating)
-	'python3-mako', # mako for python 3 (we are not really using it)
-	'python-mako-doc', # documentation for the template preprocessor
-	
-	# debian tools
-	'devscripts', # for debuild(1)
-]
-
-args=[
-	'sudo',
-	'apt-get',
-	'install',
-	'--assume-yes'
-]
-args.extend(packs)
-subprocess.check_call(args)
-
-print('installing node packages...')
-subprocess.check_call([
-	'npm',
-	'install',
-], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+install_apt()
+install_node()
