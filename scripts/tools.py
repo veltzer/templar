f2d5c9066a77058d08b754560f84f3b26bf7bad5
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-16 14:11:56 +0300

    better install

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 '''
 this script will install all the required packages that you need on
 ubuntu to compile and work with this package.
@@ -7,36 +9,44 @@ import subprocess # for check_call, DEVNULL
 import yaml # for load
 import os.path # for isfile
 
-debug=False
-def msg(m):
-	if debug:
-		print(m)
+do_msg=True
+def msg(s):
+    if debug:
+        print(s)
+
+do_debug=False
+def debug(s):
+    if do_debug:
+        print(s)
 
 APT_FILE='apt.yaml'
 def install_apt():
-	# if the file is not there it is NOT an error
-	if not os.path.isfile(APT_FILE):
-		return
-	msg('installing from {0}...'.format(APT_FILE))
-	with open(APT_FILE, 'r') as stream:
-		o=yaml.load(stream)
-	packs=[ x['name'] for x in o['packages'] ]
-	args=[
-		'sudo',
-		'apt-get',
-		'install',
-		'--assume-yes'
-	]
-	args.extend(packs)
-	subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # if the file is not there it is NOT an error
+    if not os.path.isfile(APT_FILE):
+        return
+    msg('installing from {0}...'.format(APT_FILE))
+    with open(APT_FILE, 'r') as stream:
+        o=yaml.load(stream)
+    packs=[ x['name'] for x in o['packages'] ]
+    args=[
+        'sudo',
+        'apt-get',
+        'install',
+        '--assume-yes'
+    ]
+    args.extend(packs)
+    subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 NODE_FILE='package.json'
 def install_node():
-	# if the file is not there it is NOT an error
-	if not os.path.isfile(NODE_FILE):
-		return
-	msg('installing from {0}...'.format(NODE_FILE))
-	subprocess.check_call([
-		'npm',
-		'install',
-	], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # if the file is not there it is NOT an error
+    if not os.path.isfile(NODE_FILE):
+        return
+    msg('installing from {0}...'.format(NODE_FILE))
+    subprocess.check_call([
+        'npm',
+        'install',
+    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+install_apt()
+install_node()
