01daf5afff30587ee82498de054ebb4922f62e66
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-22 19:31:07 +0300

    lets debug

--- templar/debug.py
+++ templar/debug.py
@@ -5,8 +5,8 @@ centralized debugging module
 import sys
 
 # do you want to see the commands?
-opt_show_commands = False
-opt_show_messages = False
+opt_show_commands = True
+opt_show_messages = True
 
 
 def process(l, tag):
